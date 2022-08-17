332671063e65adb8e42d457034cd0a634d458a49
伊東和成 <itokazunari5375@icloud.com>
2022-08-17 23:21:42 +0900

    特定データのテスト

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -9,8 +9,19 @@ describe 'GET /tasks' do
 
     # リクエスト成功を表す200が返ってきたか確認する。
     expect(response.status).to eq(200)
-
     # 正しい数のデータが返されたか確認する。
     expect(json.length).to eq(10)
+  end
+end
+
+describe 'GET /tasks/id' do
+  it '特定のタスクを取得する' do
+    task = create(:task, name: "サンプルタスク")
+    get "/tasks/#{task.id}"
+    json = JSON.parse(response.body)
+    # # リクエスト成功を表す200が返ってきたか確認する。
+    expect(response.status).to eq(200)
+    # # 正しい数のデータが返されたか確認する。
+    expect(json["name"]).to eq(task["name"])
   end
 end
